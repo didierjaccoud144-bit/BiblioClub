c0f58ed10cabe18e80904773d5f7063adeb46cbb
didierjaccoud144-bit <didier.jaccoud.144@gmail.com>
2026-03-17 07:14:43 +0100

    Create membres_profil.py

---
+++ membres_profil.py
@@ -0,0 +1,43 @@
+# Contenu du fichier membres_profil.py (à créer sur GitHub)
+import pandas as pd
+
+# Tes 10 membres avec leurs infos fixes
+MEMBRES_FIXES = {
+    'Didier': {
+        'Avatar': 'https://storage.googleapis.com/notion-avatars/b/4/9/4f7c2a71a94d86b5155f9c42636a0d24_1603590500742.png',
+        'Téléphone': '+41791234567',
+        'Infos_Retrait': 'au bureau lundi',
+        'Position': 'Colombier'
+    },
+    'Amélie': {
+        'Avatar': 'https://storage.googleapis.com/notion-avatars/b/4/9/4f7c2a71a94d86b5155f9c42636a0d24_1603590500742.png',
+        'Téléphone': '+41797654321',
+        'Infos_Retrait': 'à mon domicile le soir',
+        'Position': 'Colombier'
+    },
+    # AJOUTE LES 8 AUTRES ICI
+    'Aline': {'Avatar': '', 'Téléphone': '', 'Infos_Retrait': '', 'Position': 'Peseux'},
+    'Bastien': {'Avatar': '', 'Téléphone': '', 'Infos_Retrait': '', 'Position': 'Bôle'},
+    'Camille': {'Avatar': '', 'Téléphone': '', 'Infos_Retrait': '', 'Position': 'Rochefort'},
+    'Damien': {'Avatar': '', 'Téléphone': '', 'Infos_Retrait': '', 'Position': 'Milvignes'},
+    'Elodie': {'Avatar': '', 'Téléphone': '', 'Infos_Retrait': '', 'Position': 'Cortaillod'},
+    'François': {'Avatar': '', 'Téléphone': '', 'Infos_Retrait': '', 'Position': 'Boudry'},
+    'Gilles': {'Avatar': '', 'Téléphone': '', 'Infos_Retrait': '', 'Position': 'Gorgier'},
+    'Hélène': {'Avatar': '', 'Téléphone': '', 'Infos_Retrait': '', 'Position': 'Colombier'},
+    'Ibrahim': {'Avatar': '', 'Téléphone': '', 'Infos_Retrait': '', 'Position': 'Colombier'},
+    'Jean': {'Avatar': '', 'Téléphone': '', 'Infos_Retrait': '', 'Position': 'Colombier'}
+}
+
+# La liste des 12 avatars Notion que tu m'as envoyés (à remplir avec tes 12 liens)
+AVATARS_LIST = [
+    'https://storage.googleapis.com/notion-avatars/b/4/9/4f7c2a71a94d86b5155f9c42636a0d24_1603590500742.png', # Avatar 1
+    # AJOUTE LES 11 AUTRES LIENS ICI
+]
+
+def get_membre_info(prenom):
+    """Retourne les infos d'un membre à partir de son prénom (depuis le dictionnaire fixe)."""
+    return MEMBRES_FIXES.get(prenom, None)
+
+def get_liste_membres_fixes():
+    """Retourne la liste des prénoms des membres fixes."""
+    return list(MEMBRES_FIXES.keys())
